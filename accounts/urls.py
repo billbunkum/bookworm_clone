35a98e66f2665953837d6c3f36a2b7e75e2f237d
Matt Smith <msmith@apaxsoftware.com>
2016-09-26 08:26:35 -0400

    Use Bootstrapmixin for authentication form.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import url
 
+from .forms import LoginForm
+
 urlpatterns = [
     url(r'^login/$',
-    'django.contrib.auth.views.login',
-    name='login'),
+        'django.contrib.auth.views.login',
+        {'authentication_form': LoginForm},
+        name='login'),
     url(r'^logout/$',
         'django.contrib.auth.views.logout',
         {'template_name': 'registration/logout.html'},
         name='logout'),
     url(r'^logout-then-login/$',
-    'django.contrib.auth.views.logout_then_login',
-    name='logout_then_login'),
+        'django.contrib.auth.views.logout_then_login',
+        name='logout_then_login'),
 ]
